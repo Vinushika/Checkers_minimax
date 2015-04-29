896b08fc04c799beef27d48bbe6fab360e65c1b3
Vinushika <stefan.schalk12@ncf.edu>
2015-04-29 10:44:13 -0400

    Updated position to use Kalmanovich's new method.

--- src/c_minimax/CheckersPosition.java
+++ src/c_minimax/CheckersPosition.java
@@ -6,21 +6,34 @@ public class CheckersPosition implements InterfacePosition {
 
     // This implementation is designed for at most 8 columns by 8 rows
     // It packs the entire position into a single long
+    
+    // Rightmost 51=cap(log2(3^32)) bits are used to store a 32 digit trinary (0..2) number
+    // Leftmost 1 bit stores the player 1 or 2
 
     private long position = 0;
     private int nC = 0;
     private int nR = 0;
+    static private long[] powerOfThree = new long[33]; // 32  +1(for player)
 
     CheckersPosition( int nC, int nR) {
         position = 0;
         this.nC = nC;
         this.nR = nR;
+        if (powerOfThree[0] != 1) setPowerOfThree();
     }
 
     CheckersPosition( InterfacePosition pos ) {
         position = pos.getRawPosition();
         nC       = pos.nC();
         nR       = pos.nR();
+        if (powerOfThree[0] != 1) setPowerOfThree();
+    }
+    
+    private void setPowerOfThree() { 
+        powerOfThree[0] = 1;
+        for( int iPow = 1 ; iPow < 33 ; iPow++ ) {
+            powerOfThree[iPow] = 3*powerOfThree[iPow-1];
+        }
     }
 
     @Override public int nC() { return nC; }
@@ -34,7 +47,8 @@ public class CheckersPosition implements InterfacePosition {
     }
 
     private int getColor( int iC, int iR ) { // 0 if transparent, 1 if white, 2 if black
-        return (int) (position >>> (((iC/2 + iR*4)*2) & 3));
+        int retVal = (int) ( ( position / powerOfThree[(8*iR+iC)/2] ) % 3 );
+        return (retVal < 0) ? retVal+3 : retVal;
     }
 
     @Override
@@ -43,20 +57,8 @@ public class CheckersPosition implements InterfacePosition {
     }
 
     private void setColor( int iC, int iR, int color ) { // 0 if transparent, 1 if white, 2 if black
-    	int colorNow = getColor(iC, iR);
-    	if (color == colorNow){
-    		return;
-    	} else if (colorNow != 0) {
-    		if (color != 0) {
-    			System.err.println("Error: trying to place one piece on another");
-    		} else {
-    			position -= (long) (color << ((iC/2 + iR*4)*2));
-    		}
-    	} else if (color == 0) {
-    		position += (long) (color << ((iC/2 + iR*4)*2));
-    	} else {
-    		System.err.println("Error: current color is out of bounds at ("+iC+","+iR+")");
-    	}
+        int oldColor = getColor(iC,iR);
+        position += (color-oldColor) * powerOfThree[(8*iR+iC)/2];
     }
 
     @Override
@@ -64,79 +66,13 @@ public class CheckersPosition implements InterfacePosition {
         if ( !(0<iPlayer && iPlayer<3) ) {
             System.err.println("Error(Connect4Position::setPlayer): iPlayer ("+iPlayer+") out of bounds!!!");
         } else {
-            int  currentPlayer = getPlayer();
-            if ( currentPlayer != iPlayer ) {
-                if (iPlayer == 1) {
-                	for (int i = 0; i < 32; i++) {
-                		int spot = (int) ((position >>> 2*i) & 3L);
-                		if (spot == 3) {
-                			position -= (1 << (2*i));
-                		} else if (spot == 2) {
-                			return;
-                		}
-                	}
-                } else if( iPlayer == 2) {
-                	for (int i = 0; i < 32; i++) {
-                		int spot = (int) ((position >>> 2*i) & 3L);
-                		if (spot == 2) {
-                			position += (1 << (2*i));
-                		} else if (spot == 3) {
-                			return;
-                		}
-                	}
-                }
-            }
+            setColor(64,0,iPlayer);
         }
     }
-    public boolean isValidMove(int i0C, int i0R, int i1C, int i1R){
-    	int iPlayer = getPlayer();
-    	int rowsMoved = i0R - i1R;
-    	int pieceColor = getColor(i0C, i0R);
-    	if (pieceColor != iPlayer){
-			System.err.println("Error: Trying to move a piece that doesn't belong to this player");
-		} else{
-			if (getColor(i1C, i1R) == 0) {
-	        	if(iPlayer == 1) {
-	        		if(rowsMoved <= 0){
-	        			return false;
-	        		} else {
-	        			if(rowsMoved == 1) return true;
-	        			else if(rowsMoved == 2){
-	        				int jumpediC = (i0C + i1C)/2;
-	        				int jumpediR = (i0R + i1C)/2;
-	        				if (getColor(jumpediC, jumpediR) == 2) {
-	        					return true;
-	        				} else return false;
-	        			}
-	        		}
-	        	} else if(iPlayer == 2) {
-	        		if(rowsMoved >= 0){
-	        			return false;
-	        		} else {
-	        			if(rowsMoved == 1) return true;
-	        			else if(rowsMoved == 2){
-	        				int jumpediC = (i0C + i1C)/2;
-	        				int jumpediR = (i0R + i1C)/2;
-	        				if (getColor(jumpediC, jumpediR) == 1) {
-	        					return true;
-	        				} else return false;
-	        			}
-	        		}
-	        	}
-	        }
-		}
-    	return false;
-    }
-    
+
     @Override
     public int getPlayer() {
-    	for (int i = 0; i < 32; i++) {
-    		int spot = (int) ((position >>> 2*i) & 3L);
-    		if      (spot == 3) return 2;
-    		else if (spot == 2) return 1;
-    	}
-    	System.err.println("Error: Checked 32 spots, didn't find a 2 or 3, no player found");
-    	return 1;
+        return getColor(64,0);
     }
 
     @Override
@@ -175,4 +111,59 @@ public class CheckersPosition implements InterfacePosition {
         return 0;
     }
 
+    public static void main(String[] args) {// testMe(String[] args) {// Unit test (incomplete)
+        CheckersPosition position = new CheckersPosition(8,8);
+        
+        for (int iR = 0; iR < 8; iR++) {
+            for (int iC = (iR+1)%2; iC < 8; iC+=2) {
+                System.out.println("(" + iC + ", " + iR + ") = " + position.getColor(iC, iR));
+            }
+        }
+        System.out.println("-------------------------");
+
+        position.setColor(0, 0, 1);
+        position.setColor(1, 0, 0);
+        position.setColor(3, 0, 1);
+        position.setColor(4, 1, 1);
+        position.setPlayer(1);
+        for (int iR = 0; iR < 8; iR++) {
+            for (int iC = (iR+1)%2; iC < 8; iC+=2) {
+                System.out.println("(" + iC + ", " + iR + ") = " + position.getColor(iC, iR));
+            }
+        }
+        System.out.println("player: " + position.getPlayer());
+        System.out.println("-------------------------");
+
+        position.setColor(1, 0, 2);
+        position.setColor(3, 0, 2);
+        position.setColor(5, 0, 2);
+        position.setColor(7, 0, 2);
+        position.setColor(0, 1, 2);
+        position.setColor(2, 1, 2);
+        position.setColor(4, 1, 2);
+        position.setColor(6, 1, 2);
+        position.setColor(1, 2, 2);
+        position.setColor(3, 2, 2);
+        position.setColor(5, 2, 2);
+        position.setColor(7, 2, 2);
+        position.setColor(0, 5, 1);
+        position.setColor(2, 5, 1);
+        position.setColor(4, 5, 1);
+        position.setColor(6, 5, 1);
+        position.setColor(1, 6, 1);
+        position.setColor(3, 6, 1);
+        position.setColor(5, 6, 1);
+        position.setColor(7, 6, 1);
+        position.setColor(0, 7, 1);
+        position.setColor(2, 7, 1);
+        position.setColor(4, 7, 1);
+        position.setColor(6, 7, 1);
+        position.setPlayer(2);
+        for (int iR = 0; iR < 8; iR++) {
+            for (int iC = (iR+1)%2; iC < 8; iC+=2) {
+                System.out.println("(" + iC + ", " + iR + ") = " + position.getColor(iC, iR));
+            }
+        }
+        System.out.println("player: " + position.getPlayer());
+    }
 }
