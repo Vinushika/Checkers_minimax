c35866c156a6a1ec99e6e12b592119581a65c16f
Vinushika <stefan.schalk12@ncf.edu>
2015-04-29 15:12:58 -0400

    Added Dalen's new code.

--- src/c_minimax/CheckersPosition.java
+++ src/c_minimax/CheckersPosition.java
@@ -6,17 +6,23 @@ public class CheckersPosition implements InterfacePosition {
 
     // This implementation is designed for at most 8 columns by 8 rows
     // It packs the entire position into a single long
-    
+
     // Rightmost 51=cap(log2(3^32)) bits are used to store a 32 digit trinary (0..2) number
     // Leftmost 1 bit stores the player 1 or 2
 
-    private long position = 0;
+    // This is the starting position
+    // The  last 3 rows are filled by Player 1
+    // The first 3 rows are filled by Player 2
+    // It's Player 1's turn.
+    private final static long STARTING_POSITION = 926508351565160L;
+
+    private long position = STARTING_POSITION;
     private int nC = 0;
     private int nR = 0;
     static private long[] powerOfThree = new long[33]; // 32  +1(for player)
 
     CheckersPosition( int nC, int nR) {
-        position = 0;
+        position = STARTING_POSITION;
         this.nC = nC;
         this.nR = nR;
         if (powerOfThree[0] != 1) setPowerOfThree();
@@ -28,8 +34,8 @@ public class CheckersPosition implements InterfacePosition {
         nR       = pos.nR();
         if (powerOfThree[0] != 1) setPowerOfThree();
     }
-    
-    private void setPowerOfThree() { 
+
+    private void setPowerOfThree() {
         powerOfThree[0] = 1;
         for( int iPow = 1 ; iPow < 33 ; iPow++ ) {
             powerOfThree[iPow] = 3*powerOfThree[iPow-1];
@@ -52,8 +58,28 @@ public class CheckersPosition implements InterfacePosition {
     }
 
     @Override
-    public void setColor( InterfaceIterator iPos, int color ) { // color is 1 if red, 2 if yellow
-        setColor( iPos.iC(), iPos.iR(), color );
+    public void setColor( InterfaceIterator iPos, int color ) { // 0 if transparent, 1 if white, 2 if black
+
+        // Initial Position
+        int iC = iPos.iC();
+        int iR = iPos.iR();
+        // Destination Position
+        int dC = iPos.dC();
+        int dR = iPos.dR();
+        // Difference in Position
+        int diffC = dC - iC;
+        int diffR = dR - iR;
+
+        // Remove the checker from the source position
+        setColor( iC, iR, 0);
+
+        // If there's a space inbetween, remove the checker there
+        if (Math.abs(diffC) > 1 && Math.abs(diffR) > 1) {
+            setColor( (diffC/2 + iC), (diffR/2 + iR), 0);
+        }
+
+        // Add the checker to the new position
+        setColor( dC, dR, color);
     }
 
     private void setColor( int iC, int iR, int color ) { // 0 if transparent, 1 if white, 2 if black
@@ -83,8 +109,24 @@ public class CheckersPosition implements InterfacePosition {
 
     @Override
     public int isWinner() {
-        // TODO Auto-generated method stub
-        return 0;
+        printMe();
+        int topRow = 0;
+        int bottomRow = nR - 1;
+
+        // Check if Player 1 won
+        for (int iC = 1; iC < nC; iC+=2) {
+            if (getColor(iC, topRow) == 1)  return 1;
+        }
+
+        // Check if Player 2 won
+        for (int iC = 0; iC < nC; iC+=2) {
+            if (getColor(iC, bottomRow) == 2) return 2;
+        }
+
+        // Otherwise the game has yet to end
+        return -1;
+
+        // TODO Check for legal draw (highly improbable)
     }
 
     @Override
@@ -95,14 +137,23 @@ public class CheckersPosition implements InterfacePosition {
 
     @Override
     public void reset() {
-        // TODO Auto-generated method stub
+        position = STARTING_POSITION;
+    }
 
+    public void clear() {
+        position = 0;
+        setPlayer(1);
     }
 
     @Override
     public int getChipCount() {
-        // TODO Auto-generated method stub
-        return 0;
+        int chipCount = 0;
+        for (int iR = 0; iR < 8; iR++) {
+            for (int iC = (iR+1)%2; iC < 8; iC+=2) {
+                if (getColor(iC, iR) > 0) chipCount++;
+            }
+        }
+        return chipCount;
     }
 
     @Override
@@ -111,9 +162,25 @@ public class CheckersPosition implements InterfacePosition {
         return 0;
     }
 
-    public static void main(String[] args) {// testMe(String[] args) {// Unit test (incomplete)
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (int iR = 0; iR < 8; iR++) {
+            for (int iC = (iR+1)%2; iC < 8; iC+=2) {
+                output.append("(" + iC + ", " + iR + ") = " + getColor(iC, iR)+"\n");
+            }
+        }
+        return output.toString();
+    }
+
+    public void printMe() {
+        System.out.println(getChipCount() + " checkers on the board!");
+        System.out.println(this.toString());
+    }
+
+    public static void test(String[] args) {// testMe(String[] args) {// Unit test (incomplete)
         CheckersPosition position = new CheckersPosition(8,8);
-        
+
         for (int iR = 0; iR < 8; iR++) {
             for (int iC = (iR+1)%2; iC < 8; iC+=2) {
                 System.out.println("(" + iC + ", " + iR + ") = " + position.getColor(iC, iR));
